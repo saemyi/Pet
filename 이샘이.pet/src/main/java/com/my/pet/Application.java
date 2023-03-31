b85bcd16904914c376038060635f5b0376b7e09c
'국종석' <'a@a.com'>
2023-03-31 18:20:50 +0900

    update settings

--- 이샘이.pet/src/main/java/com/my/pet/Application.java
+++ 이샘이.pet/src/main/java/com/my/pet/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.my.pet.dao.map")
+@MapperScan("com.my.pet.**.dao.map")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
